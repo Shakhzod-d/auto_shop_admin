528da47cc666cb81bb9440e79d4859fd2f44a13e
Behruz Pardayev <150518431+bekhruzbekDev@users.noreply.github.com>
2025-05-01 22:18:55 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,16 +18,16 @@ export default defineConfig({
     },
   },
 
-  // server: {
-  //   host: "0.0.0.0", // Tashqi kirishlarni qabul qilish
-  //   strictPort: true, // Agar port band bo‘lsa, boshqa portga o‘tmaslik
-  //   watch: {
-  //     usePolling: true, // Docker ichida fayl o‘zgarishlarini kuzatish uchun
-  //   },
-  //   hmr: {
-  //     clientPort: 443, // HTTPS orqali ishlayotgan bo‘lsa
-  //     path: "/ws", // WebSocket’ni boshqa path’ga o‘tkazish mumkin
-  //   },
-  //   allowedHosts: ["admin.autoshop.uz"],
-  // },
+  server: {
+    host: "0.0.0.0", // Tashqi kirishlarni qabul qilish
+    strictPort: true, // Agar port band bo‘lsa, boshqa portga o‘tmaslik
+    watch: {
+      usePolling: true, // Docker ichida fayl o‘zgarishlarini kuzatish uchun
+    },
+    hmr: {
+      clientPort: 443, // HTTPS orqali ishlayotgan bo‘lsa
+      path: "/ws", // WebSocket’ni boshqa path’ga o‘tkazish mumkin
+    },
+    allowedHosts: ["admin.autoshop.uz"],
+  },
 });
